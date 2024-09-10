6b7f3f734bec71b2453156a535c597ee81eba373
Rachel Brown Gomez (Ray) <rgbrown21@icloud.com>
2024-09-09 22:52:30 -0400

    fetch account plan and organization data to script.js

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 let organizations = [];
 let account_plan = [];
+
+fetch('organization.csv')
+    .then(response => response.text())
+    .then(csvOrgText => {
+        Papa.parse(csvOrgText, {
+            header: true,
+            complete: function (results) {
+                organizations = results.data;
+                console.log(organizations);  // Parsed data as array of objects
+            }
+        })
+    })
+
+fetch('account_plan.csv')
+    .then(response => response.text())
+    .then(csvAccText => {
+        Papa.parse(csvAccText, {
+            header: true,
+            complete: function (results) {
+                account_plan = results.data;
+                console.log(account_plan);  // Parsed data as array of objects
+            }
+        })
+    })
