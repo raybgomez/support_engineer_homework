091e922e5be51ff56dc64d48f684749cfc26e1ef
Rachel Brown Gomez (Ray) <rgbrown21@icloud.com>
2024-09-11 18:05:51 -0400

    complete getOptimizationSettings

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ fetch('organization.csv')
             header: true,
             complete: function (results) {
                 organizations = results.data;
-                console.log(organizations);  // Parsed data as array of objects
+                console.log(organizations);
             }
         })
     })
@@ -20,7 +20,19 @@ fetch('account_plan.csv')
             header: true,
             complete: function (results) {
                 account_plan = results.data;
-                console.log(account_plan);  // Parsed data as array of objects
+                console.log(account_plan);
             }
         })
-    })
+    });
+
+const getOptimizationSettings = () => {
+    const domainValue = document.getElementById('myShopifyDomain').value;
+    const organization = organizations.find(org => org.myShopifyDomain.toLowerCase() === domainValue.toLowerCase());
+
+    const output = document.getElementById('reportOutput1');
+    if (organization) {
+        output.innerHTML = `<p>Below are the optimization settings for ${domainValue}: ${organization.setup.slice(-91)}</p>`
+    } else {
+        output.innerHTML = `<p>Not found</p>`
+    }
+}
